2df47b84e3ccaa879acae821d48b5ccfb2a6fb79
unknown <pelkastudent@gmail.com>
2021-10-16 17:07:02 +0200

    cwiczenie1

--- cwiczenie1.sql
+++ cwiczenie1.sql
@@ -47,15 +47,38 @@ FROM buildings ORDER BY ST_Area(geometry) DESC LIMIT 2;
 SELECT ST_Distance(buildings.geometry, poi.geometry) AS NajkrotszaOdleglosc 
 FROM buildings, poi WHERE buildings.name = 'BuildingC' AND poi.name = 'G';
 
+SELECT ST_ShortestLine(buildings.geometry, poi.geometry)
+FROM buildings, poi WHERE buildings.name = 'BuildingC' AND poi.name = 'G'
+union 
+SELECT buildings.geometry FROM buildings WHERE buildings.name = 'BuildingC'
+union 
+SELECT poi.geometry FROM poi WHERE poi.name = 'G';
+
 -- f) Wypisz pole powierzchni tej czesci budynku BuildingC, ktora znajduje sie w odleglosci wiekszej niz 0.5 od budynku BuildingB:
 SELECT ROUND(CAST(ST_Area(ST_Difference(bC.geometry, ST_Buffer(bB.geometry, 0.5))) AS numeric), 4) AS PoleWycinka
 FROM buildings AS bB, buildings AS bC WHERE bB.name = 'BuildingB' AND bC.name = 'BuildingC';
+
+SELECT ST_Area(
+			   (SELECT ST_Difference(geometry, 
+									 (SELECT ST_Buffer(geometry, 0.5)
+									  FROM buildings WHERE name = 'BuildingB'))
+			   FROM buildings WHERE name = 'BuildingC'));
+									 
+
 
 -- g) Wybierz te budynki ktorych centroid (ST_Centroid) znajduje sie powyzej drogi o nazwie RoadX. 
 SELECT buildings.name, ST_AsText(buildings.geometry) FROM buildings, roads 
 WHERE roads.name = 'RoadX' AND ST_Y(ST_Centroid(buildings.geometry)) > ST_Y(ST_Centroid(roads.geometry));
 
+SELECT buildings.name, ST_AsText(buildings.geometry) FROM buildings
+WHERE ST_Y(ST_Centroid(buildings.geometry)) > (SELECT ST_Y(ST_Centroid(geometry))
+										FROM roads WHERE roads.name = 'RoadX');
+										
+
 -- h) Oblicz pole powierzchni tych czesci budynku BuildingC i poligonu o wspolrzednych (4 7, 6 7, 6 8, 4 8, 4 7), ktore nie sa wspolne dla tych dwoch obiektow
+SELECT ST_Area(ST_SymDifference(geometry, ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'))) 
+FROM buildings WHERE name = 'BuildingC';
+
 SELECT ST_Area(ST_Difference(geometry, ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'))) AS WycinekC, ST_Area(ST_Difference(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry))  AS WycinekPoligonu,
 ST_Area(ST_Union(ST_Difference(geometry, ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))')), ST_Difference(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry))) AS SumaWyznaczonegoObszaru
 FROM buildings WHERE name = 'BuildingC';
@@ -63,3 +86,9 @@ FROM buildings WHERE name = 'BuildingC';
 SELECT ST_Area(ST_Union(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry))  AS SumaPoligonow, ST_Area(ST_Intersection(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry)) AS wycinek,
 ST_Area(ST_Difference(ST_Union(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry), ST_Intersection(ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))'), geometry))) AS SumaWyznaczonegoObszaru
 FROM buildings WHERE name = 'BuildingC';
+
+SELECT * FROM buildings
+UNION
+SELECT * FROM roads
+UNION
+SELECT * FROM poi
